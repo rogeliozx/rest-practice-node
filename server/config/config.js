ca0284d6ae47190c722b1421599cbd5e5aa821df
rogelioltz <rogelioltz@gmail.com>
2020-02-14 21:51:27 -0700

    feat: env Var

--- server/config/config.js
+++ server/config/config.js
@@ -7,7 +7,7 @@ let urlDB;
 if(process.env.NODE_ENV==='dev'){
     urlDB='mongodb://localhost:27017/review';
 }else{
-urlDB='mongodb+srv://rogelioltz:Naruto01@restserver-hm6dd.mongodb.net/test?retryWrites=true&w=majority'
+urlDB=rocess.env.MONGO_URI;
 }
 process.env.URLDB=urlDB;
 
